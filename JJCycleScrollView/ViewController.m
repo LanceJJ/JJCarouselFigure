1b6b3fef6fb1f0e6febfc9414a6fcbeae89fb806
LanceJJ <LanceJJ@163.com>
2019-11-03 15:19:49 +0800

    重构

--- JJCycleScrollView/ViewController.m
+++ JJCycleScrollView/ViewController.m
@@ -20,6 +20,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    
     [self setupCycleScrollViewType1];
     [self setupCycleScrollViewType2];
     [self setupCycleScrollViewType3];
